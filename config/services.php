d79f61ff271c71b02d1f3d1a8827b40e23db7f4d
mrglgrlgr <takememars@gmail.com>
2025-06-15 17:01:31 +0800

    ran npm run build

--- config/services.php
+++ config/services.php
@@ -35,8 +35,8 @@ return [
         ],
     ],
     'google_cloud' => [
-        'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'snap-csg-229610'),
-        'bucket'     => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'snapp_files'),
+        'project_id' => env('GOOGLE_CLOUD_PROJECT_ID'),
+        'bucket'     => env('GOOGLE_CLOUD_STORAGE_BUCKET'),
         'key_file'   => storage_path('app/credentials/snap-csg-229610-f2bb5c5c6b21.json'),
     ],
 ];
